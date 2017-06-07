c55fd27e9e957242d9896e68a4f916b57d755f31
Kendall Gillies <kendall.a.gillies@gmail.com>
2017-06-07 17:19:50 -0500

    Update Base_File_NFL_Stats.py after adding Functions_Needed_For_All_Stats.py
    Removed unnecessary packages.
    Removed the Check_for_Stats_Webpage function since it is now in Functions_Needed_For_All_Stats.py.
    Now prints after every 100 players instead of 10.

--- Base_File_NFL_Stats.py
+++ Base_File_NFL_Stats.py
@@ -1,5 +1,3 @@
-import bs4, requests
-import pandas as pd
 from Website_to_CSV_Functions import *
 from Player_Class import *
 
@@ -14,18 +12,6 @@ def Get_Players_and_Ids_From_CSV_File(filename):
                 Players_temp[player.player_id]=player
     return Players_temp
 
-# NFL does not have stats for all players
-def Check_for_Stats_Webpage(player,stats_type):
-    profile_url = 'http://www.nfl.com/player/'+player.player_id+'/profile'
-    res = requests.get(profile_url)
-    soup = bs4.BeautifulSoup(res.text,'lxml')    
-    
-    Has_Stats_Page = False
-    for div in soup.find_all('div',{'id':'player-profile-tabs'}):
-        if stats_type in div.text:
-            Has_Stats_Page = True
-    return Has_Stats_Page
-
 # Only one should be executed based on if you want to get the player names and
 # ids from a csv file or from the website.
 Players = Get_and_Store_All_Players_Names_and_Ids('Player_Ids_Urls.csv')
@@ -37,7 +23,7 @@ count = 1
 for player_id in Players:
     player = Player_Basic_Stats(Players[player_id])
     player.Get_and_Store_Basic_Stats('Basic_Stats.csv')
-    if count % 10 == 0:
+    if count % 100 == 0:
         print('Processed basic stats for %d out of %d players' % (count,len(Players)))
     count+=1
     
@@ -47,7 +33,7 @@ for player_id in Players:
     player = Career_Stats(Players[player_id])
     if Check_for_Stats_Webpage(player,'Career Stats'):
         player.Get_and_Store_Career_Stats()
-    if count % 10 == 0:
+    if count % 100 == 0:
         print('Processed career stats for %d out of %d players' % (count,len(Players)))
     count+=1
 
@@ -57,7 +43,7 @@ for player_id in Players:
     player = Game_Logs(Players[player_id])
     if Check_for_Stats_Webpage(player,'Game Logs'):
         player.Get_and_Store_Game_Logs()
-    if count % 10 == 0:
+    if count % 100 == 0:
         print('Processed game logs for %d out of %d players' % (count,len(Players)))
     count+=1
 
